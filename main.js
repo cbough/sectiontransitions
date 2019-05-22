cacca3b73ec337fdb7ab5d2da5a5e88f1e349275
Christopher Bough <christopherbough85@gmail.com>
2019-05-22 13:01:45 -0400

    final version

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-window.onscroll = function() {showMe()};
+let showContent = document.getElementsByTagName('p')
+let showContent2 = document.getElementsByTagName('img')
 
-function showMe() {
-  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-    document.getElementsByClassName("hidden").className = "show";
-  }
+ window.addEventListener('scroll', function() {
+console.log(window.scrollY)
+let reveal = window.scrollY
+
+ if (reveal > 50) {
+ showContent[0].classList.remove('hidden')
+ showContent[0].classList.add('show')
+ showContent2[0].classList.remove('hidden')
+ showContent2[0].classList.add('show')
+ }
+ if(reveal > 100){
+ showContent[1].classList.remove('hidden')
+ showContent[1].classList.add('show')
+ showContent2[1].classList.remove('hidden')
+ showContent2[1].classList.add('show')
+ }
+ if(reveal > 1350){
+ showContent[2].classList.remove('hidden')
+ showContent[2].classList.add('show')
+ showContent2[2].classList.remove('hidden')
+ showContent2[2].classList.add('show')
+} if(reveal > 1900){
+ showContent[3].classList.remove('hidden')
+ showContent[3].classList.add('show')
+ showContent2[3].classList.remove('hidden')
+ showContent2[3].classList.add('show')
 }
+ })
+
+ console.log(reveal)
